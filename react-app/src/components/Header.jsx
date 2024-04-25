7cd46099d4a650b831ab8dd61ca9d1dd0403f9a7
AKSHAT KEDIA <akshat_kedia@kedias-MacBook-Pro.local>
2024-04-25 12:56:20 +0530

    second

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,12 +1,15 @@
 
-
+import {Link} from 'react-router-dom';
+import './Header.css'
 
 
 function Header(){
-
     return(
-        <div>
-            This is HEader filesghfmghj
+        <div className="header">
+                   <Link to="/">HomePage</Link>
+
+           <span className="mt-3">This is Header filesghfmghj</span> 
+       <Link to="/login">LOGIN</Link>
         </div>
     )
 }
---
+++ react-app/src/components/Signup.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import Header from "./Header";
+
+
+function Signup() {
+    return(
+    <div>
+        <Header/>
+        Welcome to signup page...
+        <br></br>
+        USERNAME
+        <input type="text"/>
+        <br></br>
+        PASSWORD
+      <input type="text"/>
+      <br></br>
+      <button> Signup </button>
+      <Link to="/login"> LOGIN</Link>
+    </div>
+    )
+}
+
+export default Signup;
