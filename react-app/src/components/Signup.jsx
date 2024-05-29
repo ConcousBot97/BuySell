2a48c4ce114df34293034b2d6e53772895b5aceb
mansi <mansi.20223144@mnnit.ac.in>
2024-05-29 11:19:58 +0530

    m1

--- react-app/src/components/Signup.jsx
+++ react-app/src/components/Signup.jsx
@@ -1,23 +1,23 @@
-import { Link } from "react-router-dom";
-import Header from "./Header";
+// import { Link } from "react-router-dom";
+// import Header from "./Header";
 
 
-function Signup() {
-    return(
-    <div>
-        <Header/>
-        Welcome to signup page...
-        <br></br>
-        USERNAME
-        <input type="text"/>
-        <br></br>
-        PASSWORD
-      <input type="text"/>
-      <br></br>
-      <button> Signup </button>
-      <Link to="/login"> LOGIN</Link>
-    </div>
-    )
-}
+// function Signup() {
+//     return(
+//     <div>
+//         <Header/>
+//         Welcome to signup page...
+//         <br></br>
+//         USERNAME
+//         <input type="text"/>
+//         <br></br>
+//         PASSWORD
+//       <input type="text"/>
+//       <br></br>
+//       <button> Signup </button>
+//       <Link to="/login"> LOGIN</Link>
+//     </div>
+//     )
+// }
 
-export default Signup;
+// export default Signup;
