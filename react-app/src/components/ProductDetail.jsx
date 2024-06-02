832ba3cecad478b33f20001607585d9e41627086
AKSHAT KEDIA <akshat_kedia@kedias-MacBook-Pro.local>
2024-06-03 01:16:06 +0530

    3

--- react-app/src/components/ProductDetail.jsx
+++ react-app/src/components/ProductDetail.jsx
@@ -5,10 +5,9 @@ import Header from "./Header";
 
 
 function ProductDetail(){
-    const [product , setproduct] = useState()
 
+    const [product , setproduct] = useState()
     const p=useParams()
-    console.log(p.productId)
 
     useEffect(() => {
         const url = 'http://localhost:4000/get-product/' + p.productId;
@@ -26,21 +25,24 @@ function ProductDetail(){
     }, [])
 
 
-    return (
-        <div>
+    return (<>
             <Header/>
         PRODUCTS DETAILS :  
-       { product && <div>
+        <div >
+       { product && <div className="d-flex justify-content-between flex-wrap ">
             <div>
             <img width="700px" height="300px" src={'http://localhost:4000/'+product.pimage} alt="" />
            <h6>PRODUCT DETAILS : </h6>
             {product.pdesc}
             </div>
             <div>
-            {product.pname}
+            <h3 className="m-2 price-text"> Rs. {product.price} /-</h3>
+                            <p className="m-2"> {product.pname} | {product.category} </p>
+                            <p className="m-2 text-success"> {product.pdesc}</p>
             </div>
         </div>}
         </div>
+        </>
     )
 }
 export default ProductDetail;
